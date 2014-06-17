b171b80026e8a10614bd8121551c6904923b6225
Javier Murillo <jhmmurillo@gmail.com>
2014-06-16 23:49:34 -0500

    add Conekta.api_key to application.rb to load the key, you need to add conekta_key in config/secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'conekta'
 
-# Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module HelloConektaRubyRails
   class Application < Rails::Application
+    Conekta.api_key = Rails.application.secrets.conekta_key
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
