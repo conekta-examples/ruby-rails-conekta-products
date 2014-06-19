ed09cbc80f0e7a678752ef463c7754140c097b08
Javier Murillo <jhmmurillo@gmail.com>
2014-06-18 22:42:34 -0500

    condition to verify if is a subscription or if is only one charge

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -24,14 +24,25 @@ class ChargesController < ApplicationController
   # POST /charges.json
   def create
     product = Product.find(charge_params["charge"]["product_id"])
-    begin
-      charge = Conekta::Charge.create({
+    new_charge = if product.is_subscription?
+                   {
         amount: product.price.to_i,
         currency: 'MXN',
         description: product.description,
         card: charge_params["charge"]["token"],
         reference_id: "ANYIDCAN-be-ref"
-      })
+      }
+                 else
+                   {
+        amount: product.price.to_i,
+        currency: 'MXN',
+        description: product.description,
+        card: charge_params["charge"]["token"],
+        reference_id: "ANYIDCAN-be-ref"
+      }
+    end
+    begin
+      charge = Conekta::Charge.create(new_charge)
     rescue Conekta::Error => e
       flash[:error] = e.message_to_purchaser
       redirect_to buy_product_path(product.id)
